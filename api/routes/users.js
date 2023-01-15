31ef3dfa336a8822786ed8a2937afec11d74013d
akhilsuryam <akhilsuryam12@gmail.com>
2023-01-16 02:28:08 +0530

    routing

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,34 +6,89 @@ const { route } = require("./auth");
 const verify = require("../verifyToken")
  
 
+//UPDATEV) 
+// router.put("/:id", verify, async (req, res) => {
+//   if (req.user.id === req.params.id || req.user.isAdmin) {
+//     if (req.body.password) {
+//       req.body.password = CryptoJS.AES.encrypt(
+//         req.body.password,
+//         process.env.SECRET_KEY
+//       ).toString();
+//     }
 
-//update
+//     try {
+//       const updatedUser = await User.findByIdAndUpdate(
+//         req.params.id,
+//         {
+//           $set: req.body,
+//         },
+//         { new: true }
+//       );
+//       res.status(200).json(updatedUser);
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   } else {
+//     res.status(403).json("You can update only your account!");
+//   }
+// });
+
+
+//UPDATE 
 router.put("/:id", verify, async (req, res) => {
-  if (req.user.id === req.params.id || req.user.isAdmin) {
-    if (req.body.password) {
+  if (req.user && (req.user.id === req.params.id || req.user.isAdmin)) {
+      if (req.body.password) {
       req.body.password = CryptoJS.AES.encrypt(
-        req.body.password,
-        process.env.SECRET_KEY
+          req.body.password,
+          process.env.SECRET_KEY
       ).toString();
-    }
+      }
 
-    try {
+      try {
       const updatedUser = await User.findByIdAndUpdate(
-        req.params.id,
-        {
+          req.params.id,
+          {
           $set: req.body,
-        },
-        { new: true }
+          },
+          { new: true }
       );
       res.status(200).json(updatedUser);
-    } catch (err) {
+      } catch (err) {
       res.status(500).json(err);
-    }
+      }
   } else {
-    res.status(403).json("You can update only your account!");
+      res.status(403).json("You can update only your account!");
   }
 });
 
+//
+router.delete("/:id",verify,async(req,res)=>{
+  if (req.user && (req.user.id === req.params.id || req.user.isAdmin)) {
+  
+    try {
+    await User.findByIdAndDelete(req.params.id); 
+    res.status(200).json(updatedUser);
+    } catch (err) {
+    res.status(500).json(err);
+    }
+} else {
+    res.status(403).json("You can update only your account!");
+}  
+});
+//GET
+router.delete("/:id",verify,async(req,res)=>{
+  if (req.user && (req.user.id === req.params.id || req.user.isAdmin)) {
+  
+    try {
+    await User.findByIdAndDelete(req.params.id); 
+    res.status(200).json(updatedUser);
+    } catch (err) {
+    res.status(500).json(err);
+    }
+} else {
+    res.status(403).json("You can update only your account!");
+}  
+});
 module.exports = router;
 
 
--- api/verifyToken.js
+++ api/verifyToken.js
@@ -16,3 +16,6 @@ function verify(req, res, next) {
 }
 
 module.exports = verify;
+
+
+/* This is a Node.js script that exports a "verify" function which can be used as middleware in a route handler to verify a JSON web token (JWT) passed in the "token" header of an incoming HTTP request. The function first checks if the "token" header is present in the request headers, and if not, sends a 401 "Unauthenticated" response to the client. If the header is present, it splits the header value by space and takes the second element (the actual token) and verifies it using the jsonwebtoken library's "verify" method. The secret key used for verification is taken from the process.env.SECRET_KEY environment variable. If the token is valid, the payload of the token (i.e the "user" object) is attached to the request object as "req.user" which can be used later in the request's lifecycle. If the token is invalid, it sends a 403 "Forbidden" response to the client. Finally, the function calls the next middleware function in the route handler. */
